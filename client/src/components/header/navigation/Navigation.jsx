e71620dee6e98715a486e35c322072f5861631ba
Nikola Kolev <nickkolev06@abv.bg>
2024-07-28 16:48:18 +0300

    Links in navigation

--- client/src/components/header/navigation/Navigation.jsx
+++ client/src/components/header/navigation/Navigation.jsx
@@ -1,45 +1,52 @@
-import { blue } from "@mui/material/colors";
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Navigation({ isHome }) {
+    const location = useLocation();
+
     return (
         <header className="header_section" style={!isHome ? { backgroundColor: '#002049' } : {}}>
             <div className="container-fluid">
                 <nav className="navbar navbar-expand-lg custom_nav-container ">
-                    <a className="navbar-brand" href="index.html">
-                        <span>
-                            Soft Market
-                        </span>
-                    </a>
+                    <Link className="navbar-brand" to="/">
+                        <span>Soft Market</span>
+                    </Link>
 
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className=""> </span>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav  ">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
+                        <ul className="navbar-nav">
+                            <li className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}>
+                                <Link className="nav-link" to="/">
+                                    Home {location.pathname === '/' && <span className="sr-only">(current)</span>}
+                                </Link>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="about.html"> About</a>
+                            <li className={`nav-item ${location.pathname === '/software' ? 'active' : ''}`}>
+                                <Link className="nav-link" to="/software">
+                                    Catalog {location.pathname === '/software' && <span className="sr-only">(current)</span>}
+                                </Link>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="service.html">Services</a>
+                            <li className={`nav-item ${location.pathname === '/software/create' ? 'active' : ''}`}>
+                                <Link className="nav-link" to="/software/create">
+                                    Upload {location.pathname === '/software/create' && <span className="sr-only">(current)</span>}
+                                </Link>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="why.html">Why Us</a>
+                            <li className={`nav-item ${location.pathname === '/login' ? 'active' : ''}`}>
+                                <Link className="nav-link" to="/login">
+                                    Login {location.pathname === '/login' && <span className="sr-only">(current)</span>}
+                                </Link>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="team.html">Team</a>
+                            <li className={`nav-item ${location.pathname === '/register' ? 'active' : ''}`}>
+                                <Link className="nav-link" to="/register">
+                                    Register {location.pathname === '/register' && <span className="sr-only">(current)</span>}
+                                </Link>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#"> <i className="fa fa-user" aria-hidden="true"></i> Login</a>
+                            <li className={`nav-item ${location.pathname === '/profile' ? 'active' : ''}`}>
+                                <Link className="nav-link" to="/profile">
+                                    <i className="fa fa-user" aria-hidden="true"></i> Profile {location.pathname === '/profile' && <span className="sr-only">(current)</span>}
+                                </Link>
                             </li>
-                            <form className="form-inline">
-                                <button className="btn  my-2 my-sm-0 nav_search-btn" type="submit">
-                                    <i className="fa fa-search" aria-hidden="true"></i>
-                                </button>
-                            </form>
                         </ul>
                     </div>
                 </nav>
