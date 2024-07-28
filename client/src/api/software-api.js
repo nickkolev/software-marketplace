440c75b4b6d800ee0720c70933aa3a4c63d37910
Nikola Kolev <nickkolev06@abv.bg>
2024-07-28 17:11:30 +0300

    implemented abstract requester, auth and software apis

---
+++ client/src/api/software-api.js
@@ -0,0 +1,27 @@
+import * as request from './requester';
+
+const BASE_URL = 'http://localhost:3030/data/softwares';
+
+export const getAll = async () => {
+    const result = await request.get(BASE_URL);
+
+    const softwares = Object.values(result);
+
+    return softwares;
+}
+
+export const getOne = async (softwareId) => {
+    const software = await request.get(`${BASE_URL}/${softwareId}`);
+
+    return software;
+}
+
+export const create = (softwareData) => request.post(`${BASE_URL}`, softwareData);
+
+const softwaresApi = {
+    getOne,
+    getAll,
+    create,
+}
+
+export default softwaresApi;
